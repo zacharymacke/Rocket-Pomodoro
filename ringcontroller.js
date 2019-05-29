c9671d2f57e40d3f0dcff52474994b7253db3cb4
Zach Macke <beep@Zachs-MacBook-Pro.local>
2019-05-29 15:41:59 -0500

    init

---
+++ ringcontroller.js
@@ -0,0 +1,113 @@
+import { callbackify } from "util";
+
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+//global interval, holds all global timers
+let placeHolderTime, interval, timerOn;
+let circle = document.getElementById('timer');
+let radius = circle.r.baseVal.value;
+let circumference = radius * 2 * Math.PI;
+let studyTime = 30;
+let breakTime = 5;
+let numRounds = 2;
+// set the strokeDasharry settings utilizing circumference
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDasharrayoffset = `${circumference}`;
+
+window.onload = function(){
+	init();
+};
+
+function init(){
+  updateMinutes("minuteRange", 'minuteDisplay');
+  updateMinutes("breakRange", 'breakDisplay');
+  updateMinutes("roundRange", 'roundDisplay');
+  //reset interval if exists
+  clearInterval(interval);
+  setProgress(100);
+  setTimer(false);
+}
+
+//document.getElementById("menubar").style.display = 'inline-block'; 
+
+var rocketButton = document.getElementById("rocketbutton").addEventListener('click', studyCycle);
+
+var menu = document.getElementById('rocketOpenButton').addEventListener('click', function(){
+  document.getElementById("menubar").style.display = 'block'; 
+  document.getElementById("rocketOpenButton").style.display = "none";
+});
+
+var rocketclose = document.getElementById('rocketclosebutton').addEventListener('click', function(){
+  document.getElementById("menubar").style.display = "none"; 
+  document.getElementById("rocketOpenButton").style.display = "inline-block";
+});
+
+var setTime = document.getElementById("setTime").addEventListener('click', function(){
+  studyTime = document.getElementById('minuteRange').value;
+  placeHolderTime = document.getElementById('minuteRange').value;
+  breakTime = document.getElementById('breakRange').value;
+  numRounds = document.getElementById('roundRange').value;
+  document.getElementById("timeLeft").innerHTML = studyTime + ":00";
+  document.getElementById("menubar").style.display = "none"; 
+  document.getElementById("rocketOpenButton").style.display = "inline-block";
+  init();
+})
+
+function updateMinutes(range, choice){
+  // monitor sliders
+  var slider = document.getElementById(range);
+  var output = document.getElementById(choice);
+  slider.oninput = function(){
+    if(range === 'roundRange'){
+      output.innerHTML = this.value + ".0";
+    }else{
+      output.innerHTML = this.value + ":00";
+    }
+  }
+}
+
+function studyCycle(){
+  /*  set timeOn disable start button 
+      (don't want to triger a second interval) */
+      // separate study and break calls
+  setTimer(true);
+  var minutes = 0;
+  var seconds = 4;
+  interval = window.setInterval(function(){
+    seconds--
+    console.log(seconds);
+    if(seconds == 0){
+      if(minutes == 0){
+        clearInterval(interval);
+      }else{
+        minutes--;
+        seconds = 60;
+      }
+    }
+    // fix leading 0's
+    let adjustedTime = fixTime(minutes, seconds);
+    document.getElementById("timeLeft").innerHTML = adjustedTime;
+    //update ring
+    let totSeconds = (minutes * 60) + seconds;
+    let percent = (totSeconds / (placeHolderTime * 60));
+    setProgress((1-percent)*100);
+  }, 1000);
+}
+
+function fixTime(minutes, seconds){
+      //fix leading 0
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return (minutes + ':' + seconds);
+}
+
+function setProgress(percent) {
+  const offset = circumference - percent / 100 * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
+function setTimer(status){
+  timerOn = status;
+  document.getElementById("rocketbutton").disabled = status;
+}
